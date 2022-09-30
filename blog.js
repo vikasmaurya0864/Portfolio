3ae5bb82a2b9bed531b447d2f93fe56d539bdcd1
Vikas Maurya <86638724+vikasmaurya0864@users.noreply.github.com>
2022-09-30 19:45:26 +0530

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,30 @@
+function toggleHide() {
+  let btn1 = document.getElementById('btn1');
+  let para1 = document.getElementById('para1');
+  if (para1.style.display != 'none') {
+    para1.style.display = "none";
+  }
+  else {
+    para1.style.display = 'block';
+  }
+}
+function myFunction() {
+  let btn2 = document.getElementById('btn2');
+  let para2 = document.getElementById('para2');
+  if (para2.style.display != 'none') {
+    para2.style.display = "none";
+  }
+  else {
+    para2.style.display = 'block';
+  }
+}
+function toggleAide() {
+  let btn3 = document.getElementById('btn3');
+  let para3 = document.getElementById('para3');
+  if (para3.style.display != 'none') {
+    para3.style.display = "none";
+  }
+  else {
+    para3.style.display = 'block';
+  }
+}
